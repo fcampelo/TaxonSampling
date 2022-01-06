01308405c81dcb0630eab4642d380949da823117
fcampelo <fcampelo@gmail.com>
2022-01-06 23:24:53 +0000

    Update test.R

--- inst/utils/test.R
+++ inst/utils/test.R
@@ -1,8 +1,18 @@
 library(dplyr)
 
+spp_file      <- "./data_files/metadata/TaxID2sppCounts.tsv"
+taxonomy_path <-  "data_files/taxdump/"
+ids_file      <- "data_files/metadata/TaxID2SeqID.txt"
+
+## Run once to save species counts to file spp_file
+# spp_df <- get_taxID_spp_counts(taxonomy_path = "data_files/taxdump/",
+#                                start_from_species = FALSE,
+#                                spp_file = spp_file)
+
 # t0 <- Sys.time()
-taxlist <- get_counts(taxonomy_path = "data_files/taxdump/",
-                      ids_file      = "data_files/metadata/TaxID2SeqID.txt") %>%
+taxlist <- get_counts(taxonomy_path = taxonomy_path,
+                      ids_file      = ids_file,
+                      spp_file      = spp_file) %>%
   run_TS(taxon            = 40674,
          m                = 200,
          seq_file         = "data_files/fasta/mit_vertebrata.fasta",
