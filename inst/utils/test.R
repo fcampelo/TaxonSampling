1480c5df4c392b2a3e9c4062c860761b0d5166df
fcampelo <fcampelo@gmail.com>
2022-01-08 16:41:08 +0000

    changed test routines

--- inst/utils/test.R
+++ inst/utils/test.R
@@ -1,8 +1,7 @@
-library(dplyr)
-
-spp_file      <- "./data_files/metadata/TaxID2sppCounts.tsv"
-taxonomy_path <-  "data_files/taxdump/"
+spp_file      <- "/data_files/metadata/TaxID2sppCounts.tsv"
+taxonomy_path <- "data_files/taxdump/"
 ids_file      <- "data_files/metadata/TaxID2SeqID.txt"
+seq_file      <- "data_files/fasta/mit_vertebrata.fasta"
 
 ## Run once to save species counts to file spp_file
 # spp_df <- get_taxID_spp_counts(taxonomy_path = "data_files/taxdump/",
@@ -12,17 +11,18 @@ ids_file      <- "data_files/metadata/TaxID2SeqID.txt"
 # t0 <- Sys.time()
 taxlist <- get_counts(taxonomy_path = taxonomy_path,
                       ids_file      = ids_file,
-                      spp_file      = spp_file) %>%
-  run_TS(taxon            = 40674,
-         m                = 200,
-         seq_file         = "data_files/fasta/mit_vertebrata.fasta",
-         out_file         = "results/output_TS.fasta",
-         method           = "diversity",
-         randomize        = "yes",
-         replacement      = FALSE,
-         ignoreIDs        = 10090,
-         requireIDs       = 9606,
-         sampling         = "known_species")
+                      spp_file      = spp_file)
+taxlist <- run_TS(taxlist,
+                  taxon            = 40674,
+                  m                = 200,
+                  seq_file         = seq_file,
+                  out_file         = NULL,
+                  method           = "diversity",
+                  randomize        = "yes",
+                  replacement      = FALSE,
+                  ignoreIDs        = NULL,
+                  requireIDs       = NULL,
+                  sampling         = "known_species")
 
 # dt <- Sys.time() - t0
 # message("\nElapsed time: ", dt, " ", units(dt))
